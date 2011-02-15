ff30210d6ec207215e6945001845f5cd0f71dc0c
Jeremy Wright <jwright@hemispheregps.com>
2011-02-15 03:21:05 -0700

    Solution Complete.

--- InputFile.cpp
+++ InputFile.cpp
@@ -39,8 +39,7 @@ bool cpp_practicum::InputFile::next()
 	 * if(find()'s index is outside the bounds of string)...
 	 */
 
-	if(line.length() == _file_format.get_sentenial_value().length()+1 && // Add 1 to get the end line too.
-	   line.find(ss.str()) != string::npos)
+	if(line.find(ss.str()) != string::npos)
 	{
 		return false;
 	}
