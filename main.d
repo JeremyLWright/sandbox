da721e461274161c6e33149bba4bc9b6d636729e
Jeremy Wright <jwright@hemispheregps.com>
2011-05-11 13:09:51 -0700

    Added the less trivial main.d for testing.

--- main.d
+++ main.d
@@ -1,7 +1,36 @@
+#!/usr/bin/dmd -run
+/**
+ * Version: Something Awesome
+ */
 import std.stdio;
 
-int main(string[] args)
-{
-    writefln("Hello World");
+char flags[8191];
+/** 
+ * Authors: Jeremy Wright
+ * Version: Some
+ */
+int main()
+{   int     i, prime, k, count, iter;
+version(D_Coverage) {}
+else { writefln("10 iterations"); }
+    for (iter = 1; iter <= 10; iter++)
+    {	count = 0;
+	flags[] = true;
+	for (i = 0; i < flags.length; i++)
+	{   if (flags[i])
+	    {	prime = i + i + 3;
+		k = i + prime;
+		while (k < flags.length)
+		{
+		    flags[k] = false;
+		    k += prime;
+		}
+		count += 1;
+	    }
+	}
+    }
+    version(D_Coverage) {}
+else { writefln("%d primes", count);}
     return 0;
 }
+
---
+++ makefile
@@ -0,0 +1,19 @@
+DMD=dmd
+DOCDIR=doc
+
+#DFLAGS=-O -release -inline -nofloat -w -d -Isrc -Iimport
+DFLAGS=-unittest -g -d
+#DFLAGS=-unittest -cov -g -d
+
+SRCS=main.d
+
+all: red_black_tree
+
+.d.obj:
+	$(DMD) -c $(DFLAGS) $*
+
+.PHONY: all
+
+
+red_black_tree: $(SRCS)
+	$(DMD) $(DFLAGS) $(SRCS)
