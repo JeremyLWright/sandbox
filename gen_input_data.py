cc90afaa404eb5d4f3ade5db90e66b0660c50529
Jeremy Wright <jeremy@quiescent.us>
2011-05-27 07:21:01 -0700

    10M integer run.

--- gen_input_data.py
+++ gen_input_data.py
@@ -3,7 +3,7 @@ from sys import maxint
 
 inputfile = open("random_list.dat", "w")
 data = []
-for i in range(1000000):
+for i in range(10000000):
     data.append(uniform(1,1000000))
 for datum in data:
     inputfile.write("%d\n"%datum)
