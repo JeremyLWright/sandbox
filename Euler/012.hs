f14a9a603a87d0a199bc00fedda9e2f05e8bc70f
Jeremy Wright <jwright@quiescent.us>
2013-05-07 18:12:46 -0700

    012 is working, but takes a long time :-(

---
+++ Euler/012.hs
@@ -0,0 +1,23 @@
+import Data.List 
+
+primeFactors n = factor n primes
+    where
+        factor n (p:ps)
+            | p*p > n           = [n]
+            | n `mod` p == 0    = p: factor (n `div` p) (p:ps)
+            | otherwise         = factor n ps
+
+primes = 2 : filter ((==1) . length . primeFactors) [3,5..]
+
+divisors f = sort $ nub $ concat [(primeFactors f), (map product (tails $ primeFactors f))]
+
+triangleNumbers = map sum $ inits [1..]
+
+--take 1 filter (==500) (map length (map divisors triangleNumbers))
+nDivisors n = length $ divisors n
+
+main = print (head $ filter ((>10) . nDivisors) triangleNumbers)
+--factorLength f = filter (==f) (map length (map divisors
+
+
+
