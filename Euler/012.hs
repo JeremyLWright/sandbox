98b9185ce66d8d7f58485b5d5400e5d4e39c20b7
Jeremy Wright <jeremy@codestrokes.com>
2013-05-24 15:23:06 -0700

    Moved prime to it's own module

--- Euler/012.hs
+++ Euler/012.hs
@@ -1,13 +1,5 @@
 import Data.List 
-
-primeFactors n = factor n primes
-    where
-        factor n (p:ps)
-            | p*p > n           = [n]
-            | n `mod` p == 0    = p: factor (n `div` p) (p:ps)
-            | otherwise         = factor n ps
-
-primes = 2 : filter ((==1) . length . primeFactors) [3,5..]
+import Euler.Prime
 
 divisors f = sort $ nub $ concat [(primeFactors f), (map product (tails $ primeFactors f))]
 
