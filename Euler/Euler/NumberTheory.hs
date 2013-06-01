19a1c76640cf73066e9f0694b8f0fff837c8fd50
Jeremy Wright <jeremy@codestrokes.com>
2013-06-01 12:15:29 -0700

    Update the prime algorithm from http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf

--- Euler/Euler/NumberTheory.hs
+++ Euler/Euler/NumberTheory.hs
@@ -7,7 +7,25 @@ primeFactors n = factor n primes
             | n `mod` p == 0    = p: factor (n `div` p) (p:ps)
             | otherwise         = factor n ps
 
-primes = 2 : filter ((==1) . length . primeFactors) [3,5..]
+primesSlow = 2 : filter ((==1) . length . primeFactors) [3,5..]
+
+--Take from paper: http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
+primes = 2:([3..] `minus` composites)
+    where
+        composites = union [multiples p | p <- primes]
+        multiples n = map (n*) [n..] 
+        (x:xs) `minus` (y:ys) | x < y = x:(xs `minus` (y:ys))
+                                | x == y = xs `minus` ys
+                                | x > y = (x:xs) `minus` ys
+
+union = foldr merge []
+    where
+        merge (x:xs) ys = x:merge' xs ys
+        merge' (x:xs) (y:ys) | x < y = x:merge' xs (y:ys)
+                             | x == y = x:merge' xs ys
+                             | x > y = y:merge' (x:xs) ys
+
+
 
 divisors n = [x | x <- [1..(n-1)], n `rem` x == 0]
 --main = print $ divisors 100000000
