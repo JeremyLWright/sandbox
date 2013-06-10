a2e6856dd89cf037098158380a627eb7468b777c
Jeremy Wright <jeremy@codestrokes.com>
2013-06-10 08:57:27 -0700

    Added compile time stats to the graph

--- Euler/process.m
+++ Euler/process.m
@@ -1,7 +1,15 @@
-[names, times] = textread('times.dat', '%s %f');
-bar(times);
+[compile_names, compile_times] = textread('compile.dat', '%s %f');
+[run_names, run_times] = textread('times.dat', '%s %f');
+subplot(2,1,1)
+bar(run_times);
 colormap(summer (64))
-ylabel("Time (seconds)")
-xlabel("Problem Number")
+ylabel("Run Time (seconds)")
 title("Jeremy's Project Euler Progress")
+
+subplot(2,1,2)
+bar(compile_times)
+colormap(summer (64))
+ylabel("Compile Time (seconds)")
+xlabel("Problem Number")
 print -dpng euler.png;
+
