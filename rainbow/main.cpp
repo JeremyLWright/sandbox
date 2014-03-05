b18a5d4906433b2ade900dc710fdbfede774e16d
Jeremy Wright <jeremy@codestrokes.com>
2014-03-04 17:54:37 -0700

    Musing with rainbow tables.

--- rainbow/main.cpp
+++ rainbow/main.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <fstream>
 #include <iostream>
 #include <functional>
 #include <string>
@@ -43,7 +44,7 @@ class nss_sha256 {
             if (slot) PK11_FreeSlot(slot);
         }
 
-        std::array<uint8_t, 32> operator()(uint8_t data[], unsigned int len)
+        std::array<uint8_t, 32> operator()(uint8_t const data[], unsigned int len)
         {
             uint8_t digest[32];
 
@@ -56,9 +57,33 @@ class nss_sha256 {
             return a;
         }
 };
+
+template <size_t PASS_LEN=3>
+class hash_chain {
+    nss_sha256 sha;
+    public:
+    hash_chain()
+    {
+    }
+
+    auto hash(std::string p) -> std::array<uint8_t, 32>
+    {
+        uint8_t const * d = (uint8_t const *)p.c_str();
+        return sha(d, p.size());
+    }
+
+
+    auto reduce(std::array<uint8_t, 32> h) -> std::string
+    {
+        auto toBase26 = [](uint8_t n){ return 'a' + n%26;};
+        std::string s;
+        std::transform(std::begin(h), std::begin(h)+PASS_LEN, std::back_inserter(s), toBase26);
+        return s;
+    }
+};
+
 int main(int argc, const char *argv[])
 {
-    unsigned char data[80];
 
     /* Initialize NSS
      * If your application code has already initialized NSS, you can skip it
@@ -67,15 +92,21 @@ int main(int argc, const char *argv[])
      * require a NSS database to exist
      */
     NSS_NoDB_Init(".");
+    std::ofstream f("rainbow.3.txt");
+    hash_chain<3> hc;
+    std::string p = "abcde";
+    while(std::next_permutation(std::begin(p), std::end(p)))
+    {
+        std::string o(std::begin(p), std::begin(p)+3);
+        f << o << ":";
+        for(int i = 0; i < 100000; ++i)
+        {
+            auto h = hc.hash(o);
+            o = hc.reduce(h);
+        }
+        f << o << '\n';
+    }
 
-    /* Initialize data */
-    memset(data, 0xbc, sizeof data);
-    nss_sha256 sha;
-    auto digest = sha(data, sizeof(data));
-    printDigest(digest);
-    data[9] = '9';
-    digest = sha(data, sizeof(data));
-    printDigest(digest);
 
     return 0;
 }
